73701d7395813b3f32859d68a77b54d99fa4cefa
Hugh Osborne <hugh.osborne@gmail.com>
2025-01-02 15:37:30 +0000

    backup

--- src/OpenSimCreator/Documents/CustomComponents/NeuralModel/MotorUnitGroup.cpp
+++ src/OpenSimCreator/Documents/CustomComponents/NeuralModel/MotorUnitGroup.cpp
@@ -11,6 +11,13 @@ using namespace OpenSim;
 using namespace std;
 
 //=============================================================================
+// STATE VARIABLE NAMES
+//=============================================================================
+const std::string MotorUnitGroup::STATE_MEAN_MEMBRANE_POTENTIAL_NAME = "membrane_potentials";
+const std::string MotorUnitGroup::STATE_MEAN_FIRING_RATE_NAME = "mean_firing_rate";
+const std::string MotorUnitGroup::STATE_MUSCLE_EXCITATION_NAME= "muscle_excitation";
+
+//=============================================================================
 // CONSTRUCTOR(S) AND DESTRUCTOR
 //=============================================================================
 MotorUnitGroup::MotorUnitGroup() :
@@ -23,6 +30,17 @@ MotorUnitGroup::~MotorUnitGroup() = default;
 void MotorUnitGroup::constructProperties() {
     setAuthors("Hugh Osborne based on work by Ajay Seth, Frank Anderson, Chand John, Samuel Hamner");
     constructProperty_enabled(true);
+
+    constructProperty_num_neurons(50);
+
+    constructProperty_tau_mean(1.0);
+    constructProperty_tau_var(0.0);
+    constructProperty_rest_mean(-70.0);
+    constructProperty_rest_var(0.0);
+    constructProperty_threshold_mean(-50.0);
+    constructProperty_threshold_var(0.0);
+    constructProperty_refractory_mean(0.02); 
+    constructProperty_refractory_var(0.0);
 }
 
 //=============================================================================
@@ -34,4 +52,85 @@ bool MotorUnitGroup::isEnabled() const {
 
 void MotorUnitGroup::setEnabled(bool aTrueFalse) {
     upd_enabled() = aTrueFalse;
+}
+
+const std::vector<MotorUnitGroup::LIF_Neuron>& MotorUnitGroup::getNeurons(const SimTK::State& s) const
+{
+    if (isCacheVariableValid(s, _neurons)) {
+        return getCacheVariableValue(s, _neurons);
+    }
+
+    std::vector<MotorUnitGroup::LIF_Neuron>& neurons = updCacheVariableValue(s, _neurons);
+    updateNeurons(s, neurons);
+    markCacheVariableValid(s, _neurons);
+    return neurons;
+}
+
+std::vector<MotorUnitGroup::LIF_Neuron>& MotorUnitGroup::updNeurons(const SimTK::State& s) const
+{
+    return updCacheVariableValue(s, _neurons);
+}
+
+//==============================================================================
+// MODELCOMPONENT INTERFACE REQUIREMENTS
+//==============================================================================
+void MotorUnitGroup::extendConnectToModel(Model& model)
+{
+    Super::extendConnectToModel(model);
+}
+
+void MotorUnitGroup::extendAddToSystem(SimTK::MultibodySystem& system) const
+{
+    Super::extendAddToSystem(system);
+
+    this->_neurons = addCacheVariable("neurons", std::vector<LIF_Neuron>(get_num_neurons()), SimTK::Stage::Dynamics);
+}
+
+void MotorUnitGroup::extendInitStateFromProperties(SimTK::State& s) const
+{
+    Super::extendInitStateFromProperties(s);
+
+
+
+    if (!get_ignore_activation_dynamics()) {
+        setActivation(s, getDefaultActivation());
+    }
+    if (!get_ignore_tendon_compliance()) {
+        setFiberLength(s, getDefaultFiberLength());
+    }
+}
+
+void MotorUnitGroup::extendSetPropertiesFromState(const SimTK::State& s)
+{
+    Super::extendSetPropertiesFromState(s);
+
+    if (!get_ignore_activation_dynamics()) {
+        setDefaultActivation(getStateVariableValue(s, STATE_ACTIVATION_NAME));
+    }
+    if (!get_ignore_tendon_compliance()) {
+        setDefaultFiberLength(getStateVariableValue(s, STATE_FIBER_LENGTH_NAME));
+    }
+}
+
+void MotorUnitGroup::computeStateVariableDerivatives(const SimTK::State& s) const
+{
+    // Activation dynamics if not ignored
+    if (!get_ignore_activation_dynamics()) {
+        double adot = 0;
+        // if not disabled or overridden then compute its derivative
+        if (appliesForce(s) && !isActuationOverridden(s)) {
+            adot = getActivationDerivative(s);
+        }
+        setStateVariableDerivativeValue(s, STATE_ACTIVATION_NAME, adot);
+    }
+
+    // Fiber length is the next state (if it is a state at all)
+    if (!get_ignore_tendon_compliance()) {
+        double ldot = 0;
+        // if not disabled or overridden then compute its derivative
+        if (appliesForce(s) && !isActuationOverridden(s)) {
+            ldot = getFiberVelocity(s);
+        }
+        setStateVariableDerivativeValue(s, STATE_FIBER_LENGTH_NAME, ldot);
+    }
 }
--- src/OpenSimCreator/Documents/CustomComponents/NeuralModel/MotorUnitGroup.h
+++ src/OpenSimCreator/Documents/CustomComponents/NeuralModel/MotorUnitGroup.h
@@ -21,6 +21,14 @@ class Model;
  * a more complex model would be preferable to capture other behaviours beyond
  * tonic spiking.
  * 
+ * The LIF model tracks membrane potential that slowly reduces over time towards
+ * the resting potential. Input spikes cause instantaneous jumps in membrane
+ * potential (delta synapses). When the membrane potential goes above the
+ * threshold potential, the membrane potential is reset to the resting potential.
+ * Additional incoming spikes are ignored during the refractory period.
+ * 
+ * The refractory period is useful to impose a maximum firing rate.
+ * 
  * @author Hugh Osborne
  *
  */
@@ -36,44 +44,52 @@ public:
         "Flag (true or false) indicating whether or not the population is "
         "enabled." );
 
-    // The innervated muscle
-    OpenSim_DECLARE_SOCKET(ActivatedMuscle, Muscle, 
-        "The muscle that will have its excitation set.");
-
     // Number of motoneurons
     OpenSim_DECLARE_PROPERTY(num_neurons, int,
         "The number of motoneurons.");
 
     // LIF tau mean
-    OpenSim_DECLARE_PROPERTY(LIF_tau_mean, double,
+    OpenSim_DECLARE_PROPERTY(tau_mean, double,
         "Mean value for tau (time scale) for the LIF neurons.");
 
     // LIF tau var
-    OpenSim_DECLARE_PROPERTY(LIF_tau_var, double,
+    OpenSim_DECLARE_PROPERTY(tau_var, double,
         "Variance for tau (time scale) for the LIF neurons.");
 
     // LIF Resting Potential mean
-    OpenSim_DECLARE_PROPERTY(LIF_rest_mean, double,
+    OpenSim_DECLARE_PROPERTY(rest_mean, double,
         "Mean value for resting potential of the LIF neurons.");
 
     // LIF Resting Potential var
-    OpenSim_DECLARE_PROPERTY(LIF_rest_var, double,
+    OpenSim_DECLARE_PROPERTY(rest_var, double,
         "Variance for resting potential of the LIF neurons.");
 
     // LIF Threshold mean
-    OpenSim_DECLARE_PROPERTY(LIF_threshold_mean, double,
+    OpenSim_DECLARE_PROPERTY(threshold_mean, double,
         "Mean value for spike threshold of the LIF neurons.");
 
     // LIF Threshold var
-    OpenSim_DECLARE_PROPERTY(LIF_threshold_var, double,
+    OpenSim_DECLARE_PROPERTY(threshold_var, double,
         "Variance for spike threshold of the LIF neurons.");
 
-    // Membrane Potentials
-    OpenSim_DECLARE_LIST_OUTPUT(membrane_potentials, double, 
+    // LIF refractory time mean
+    OpenSim_DECLARE_PROPERTY(refractory_mean, double,
+        "Mean value for spike threshold of the LIF neurons.");
+
+    // LIF refractory time var
+    OpenSim_DECLARE_PROPERTY(refractory_var, double,
+        "Variance for spike threshold of the LIF neurons.");
+
+    // Average Membrane Potential
+    OpenSim_DECLARE_OUTPUT(mean_membrane_potential, double, 
         SimTK::Stage::Dynamics);
 
     // Average Firing Rate
     OpenSim_DECLARE_OUTPUT(mean_firing_rate, double,
+        SimTK::Stage::Dynamics);
+
+    // Muscle Excitation Value
+    OpenSim_DECLARE_OUTPUT(muscle_excitation, double,
         SimTK::Stage::Dynamics);
 
 
@@ -86,9 +102,6 @@ public:
     MotorUnitGroup();
     ~MotorUnitGroup() override;
 
-    //--------------------------------------------------------------------------
-    // CONTROLLER INTERFACE
-    //--------------------------------------------------------------------------
     /** Get whether or not this controller is enabled.
      * @return true when controller is enabled.
      */
@@ -99,11 +112,64 @@ public:
      */
     void setEnabled(bool enableFlag);
 
+    //==============================================================================
+    // MODELCOMPONENT INTERFACE REQUIREMENTS
+    //==============================================================================
+    /** Sets up the ModelComponent from the model, if necessary */
+    void extendConnectToModel(Model& model) override;
+
+    /** Creates the ModelComponent so that it can be used in simulation */
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override;
+
+    /** Initializes the state of the ModelComponent */
+    void extendInitStateFromProperties(SimTK::State& s) const override;
+
+    /** Sets the default state for the ModelComponent */
+    void extendSetPropertiesFromState(const SimTK::State& s) override;
+
+    /** Computes state variable derivatives */
+    void computeStateVariableDerivatives(const SimTK::State& s) const override;
+
+protected:
+    static const std::string STATE_MEAN_MEMBRANE_POTENTIAL_NAME;
+    static const std::string STATE_MEAN_FIRING_RATE_NAME;
+    static const std::string STATE_MUSCLE_EXCITATION_NAME;
+
 
 private:
 
     // Construct and initialize properties.
     void constructProperties();
+
+protected:
+
+    struct LIF_Neuron {                     //DIMENSION         Units      
+        double tau;                         //time              s  
+        double resting_potential;           //voltage           mV
+        double threshold_potential;         //voltage           mV        
+        double refractory_period;           //time              s
+        double membrane_potential;          //voltage           mV
+
+        LIF_Neuron() :
+            tau(SimTK::NaN),
+            resting_potential(SimTK::NaN),
+            threshold_potential(SimTK::NaN),
+            refractory_period(SimTK::NaN),
+            membrane_potential(SimTK::NaN),
+            {
+        }
+        friend std::ostream& operator<<(std::ostream& o,
+            const LIF_Neuron& mli) {
+            o << "MotorUnitGroup::LIF_Neuron should not be serialized!"
+                << std::endl;
+            return o;
+        }
+    };
+
+    mutable CacheVariable<std::vector<LIF_Neuron>> _neurons;
+
+    const std::vector<LIF_Neuron>& getNeurons(const SimTK::State& s) const;
+    std::vector < LIF_Neuron>& updNeurons(const SimTK::State& s) const;
 
 };  // class MotorUnitGroup
 
